f23d8f5f86805d6bc516ca6bc3d4ffa25f0345dd
Ryan Toh <ryantoh99@gmail.com>
2023-10-19 15:51:10 +0800

    move pymavlink stuff, add ros2_offboard

---
+++ ros2_offboard/src/offboard_control.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <rclcpp/rclcpp.hpp>
+#include <px4_msgs/msg/vehicle_command.hpp>
+
+int main(int argc, char ** argv)
+{
+  printf("hello world ros2_offboard package\n");
+  return 0;
+}
