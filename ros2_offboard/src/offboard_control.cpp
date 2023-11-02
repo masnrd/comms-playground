8a64834d425c3bdc4e605b428bc092abcef39593
Ryan Toh <ryantoh99@gmail.com>
2023-11-02 17:27:43 +0800

    feat: use local vehicle pos instead

--- ros2_offboard/src/offboard_control.cpp
+++ ros2_offboard/src/offboard_control.cpp
@@ -3,102 +3,198 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <px4_msgs/msg/vehicle_command.hpp>
-#include <px4_msgs/msg/vehicle_control_mode.hpp>
-#include <px4_msgs/msg/trajectory_setpoint.hpp>
 #include <px4_msgs/msg/offboard_control_mode.hpp>
+#include <px4_msgs/msg/vehicle_local_position.hpp>
+#include <px4_msgs/msg/trajectory_setpoint.hpp>
 
 using namespace std::chrono_literals;
-using namespace px4_msgs::msg;
 
-class OffboardControl : public rclcpp::Node {
-public:
-  OffboardControl() : Node("ros2_offboard")
-  {
-    target_ = TrajectorySetpoint{};
-    target_.position = {0.0, 0.0, -5.0};
-    target_.yaw = -3.14;
-    heartbeat_pub_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
-    traj_setpoint_pub_ = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
-    veh_cmd_pub_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
+using px4_msgs::msg::VehicleCommand;
+using px4_msgs::msg::OffboardControlMode; // Heartbeat
 
-    // The original code for offboard control runs this at least 10 times before arming, this however seems to work? might need to change for the real thing
-    pub_heartbeat();
+using px4_msgs::msg::VehicleLocalPosition;
+using px4_msgs::msg::TrajectorySetpoint;
 
-    this->arm();
-    this->pub_vehcom(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6); // Set to Offboard Mode
-    
-    auto timer_callback = [this]() -> void {
-      pub_heartbeat();
-      pub_target();
-    };
-    timer_ = this->create_wall_timer(100ms, timer_callback);
-  }
-  void arm();
-  void disarm();
-private:
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<OffboardControlMode>::SharedPtr heartbeat_pub_;
-  rclcpp::Publisher<TrajectorySetpoint>::SharedPtr traj_setpoint_pub_;
-  rclcpp::Publisher<VehicleCommand>::SharedPtr veh_cmd_pub_;
-  
-  TrajectorySetpoint target_;
-  
-  void pub_heartbeat();
-  void pub_target();
-  void pub_vehcom(uint16_t command, float param1 = 0.0, float param2 = 0.0);
+class Point
+{
+    public:
+    float x, y;
+    Point() { x = 0; y = 0; }
+    Point(const float initX, const float initY) {
+        x = initX;
+        y = initY;
+    }
 };
 
-void OffboardControl::arm()
+
+class OBCNode : public rclcpp::Node 
 {
-  pub_vehcom(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
-  RCLCPP_INFO(this->get_logger(), "Arm Command Sent.");
+    private:
+    uint64_t counter_; // current cycle
+
+    // Position Tracking
+    Point pos; // current X/Y
+    float alt; // current altitude
+
+    TrajectorySetpoint tgt; // target point
+    bool operating; // if the drone is in operation
+    
+    // Closeness to target (in metres) to which the drone considers itself as having "reached" the target
+    const float tolerance = 0.25;
+
+    // Target Altitude, in metres
+    const float targetAlt = 5;
+
+    // Velocity Targets (m/s)
+    const float fullSpeed = 5.0;
+    const float scanSpeed = 2.0;
+
+    // Publisher Interval
+    const std::chrono::milliseconds pubIntv = 100ms;
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<VehicleCommand>::SharedPtr vehcom_pub;
+    rclcpp::Publisher<OffboardControlMode>::SharedPtr ocm_pub;
+    rclcpp::Subscription<VehicleLocalPosition>::SharedPtr localpos_sub;
+    rclcpp::Publisher<TrajectorySetpoint>::SharedPtr tsp_pub;
+
+    void set_target(const Point pt);
+    void process_pos();
+
+    void pub_heartbeat();
+    void pub_target();
+    void pub_vehcom(uint16_t cmd, float p1 = 0.0, float p2 = 0.0);
+
+    public:
+    OBCNode();
+    void arm();
+    void disarm();
+    void log(const char* msg);
+};
+
+OBCNode::OBCNode() : Node("ros2_offboard")
+{
+    // Initial Setup
+    counter_ = 0;
+    pos = Point(0, 0);
+    alt = 0;
+    operating = false;
+    tgt = TrajectorySetpoint{}; // Setpoint message to be sent
+    tgt.position = {0.0, 0.0, -targetAlt};
+
+    // Setup Subscriber
+    // 1. QOS Setup (ROS2-PX4 interfacing issues)
+    rmw_qos_profile_t qos_sub_prof = rmw_qos_profile_sensor_data;
+    auto qos_sub = rclcpp::QoS(rclcpp::QoSInitialization(qos_sub_prof.history, 5), qos_sub_prof);
+
+    // 2. Create subscription
+    localpos_sub = this->create_subscription<VehicleLocalPosition>("/fmu/out/vehicle_local_position", qos_sub, [this](const VehicleLocalPosition::UniquePtr msg) {
+        // Update local position every time PX4 publishes to this
+        pos.x = msg->x;
+        pos.y = msg->y;
+        alt = -msg->z;
+
+        std::cout << "Position: " << pos.x << "," << pos.y << std::endl;
+
+        // If we've reached the tgt, then we can report.
+        if (operating)
+            process_pos();
+    });
+
+    // Setup Publishers
+    ocm_pub = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
+    tsp_pub = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
+    vehcom_pub = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
+
+    // Setup timed publishing
+    auto timer_callback = [this]() -> void {
+        counter_++;
+        if (counter_ <= 10) {
+            // Continuous sending for the first 1 second
+            this->arm();
+            this->pub_vehcom(
+                VehicleCommand::VEHICLE_CMD_DO_SET_MODE,
+                1, 6 // Set to OFFBOARD mode
+            );
+        }
+        pub_heartbeat();
+
+        if (operating)
+            pub_target();
+    };
+    timer_ = this->create_wall_timer(pubIntv, timer_callback);
+
+
+    // TESTER CODE
+    set_target(Point(10.0, 10.0)); log("Going to (10.0, 10.0).");
 }
 
-void OffboardControl::disarm()
-{
-  pub_vehcom(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
-  RCLCPP_INFO(this->get_logger(), "Disarm Command Sent.");
+void OBCNode::process_pos() {
+    if (abs(pos.x - tgt.position[0]) > tolerance)
+        return;
+    if (abs(pos.y - tgt.position[1]) > tolerance)
+        return;
+    
+    // If we've reached the target, report in.
+    log("Reached target.");
+
+    // TESTER CODE
+    set_target(Point(-10.0, -10.0)); log("Going to (-10.0, -10.0).");
 }
 
-void OffboardControl::pub_heartbeat()
-{
-  OffboardControlMode heartbeat{};
-  heartbeat.position = true;
-  heartbeat.velocity = false;
-  heartbeat.acceleration = false;
-  heartbeat.attitude = false;
-  heartbeat.body_rate = false;
-  heartbeat.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-  heartbeat_pub_->publish(heartbeat);
+void OBCNode::set_target(const Point pt) {
+    operating = false;
+    tgt.position = {pt.x, pt.y, -targetAlt};
+    operating = true;
 }
 
-void OffboardControl::pub_vehcom(uint16_t command, float param1, float param2)
-{
-  VehicleCommand msg{};
-  msg.param1 = param1;
-  msg.param2 = param2;
-  msg.command = command;
-  msg.target_system = 1;
-  msg.target_component = 1;
-  msg.source_system = 1;
-  msg.source_component = 1;
-  msg.from_external = true;
-  msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-  veh_cmd_pub_->publish(msg);
+void OBCNode::log(const char* msg) {
+    RCLCPP_INFO(this->get_logger(), msg);
 }
 
-void OffboardControl::pub_target()
-{
-  target_.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-  traj_setpoint_pub_->publish(target_);
+void OBCNode::arm() {
+    pub_vehcom(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
+    log("Arm command sent.");
 }
 
-int main(int argc, char ** argv)
+void OBCNode::disarm() {
+    pub_vehcom(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
+    log("Disarm command sent.");
+}
+
+void OBCNode::pub_heartbeat() {
+    OffboardControlMode hb{};
+    hb.position = true;
+    hb.velocity = false; hb.acceleration = false; hb.attitude = false; hb.body_rate = false;
+    hb.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    ocm_pub->publish(hb);
+}
+
+void OBCNode::pub_vehcom(uint16_t cmd, float p1, float p2) {
+    VehicleCommand msg{};
+    msg.param1 = p1; msg.param2 = p2;
+    msg.command = cmd;
+    msg.target_system = 1;
+    msg.target_component = 1;
+    msg.source_system = 1;
+    msg.source_component = 1;
+    msg.from_external = true;
+    msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    vehcom_pub->publish(msg);
+}
+
+void OBCNode::pub_target() {
+    // Continuously publish to this to ensure drone knows where to go
+    tgt.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    tsp_pub->publish(tgt);
+}
+
+int main(int argc, char** argv)
 {
-  std::cout << "Starting offboard control node." << std::endl;
-  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<OffboardControl>());
-  rclcpp::shutdown();
-  return 0;
+    std::cout << "Starting OBCNode." << std::endl;
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<OBCNode>());
+    rclcpp::shutdown();
+    return 0;
 }
